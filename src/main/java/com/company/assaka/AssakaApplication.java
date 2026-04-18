9c9ab22532f63b2be6c6c3e41751a2008be5aa6d
Assaka <2682728781@qq.com>
2026-04-18 15:34:37 +0800

    first commit

---
+++ src/main/java/com/company/assaka/AssakaApplication.java
@@ -0,0 +1,13 @@
+package com.company.assaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssakaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/assaka/AssakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.assaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssakaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
